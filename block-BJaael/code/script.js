2eccea8575bf22185684c83db1eec09085724d4c
aditya <agoswami0708g@gmail.com>
2022-04-14 00:56:31 +0530

    done

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -1,6 +1,3 @@
-let body = document.querySelector('body');
-let container = document.createElement('div');
-
 function disco(){
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
@@ -13,66 +10,23 @@ function random(max){
     return num;
 }
 
-container.style.display = "flex";
-container.style.flexWrap = 'wrap';
-body.append(container);
-
+let boxes = document.querySelector(".boxes")
 
 for (let i = 0; i<500; i++){
-let box = document.createElement('div') 
-box.className = "box"
-box.style.height = "4rem";
-box.style.width = "4rem"  
-box.style.border = "1px solid black";
-let p = document.createElement('p')
-p.innerText = random(500);
-box.append(p);
-container.append(box);
+let div = document.createElement('div') 
+div.classList.add("box")
+let h2 = document.createElement('h2')
+h2.innerText = random(500);
+div.append(h2);
+boxes.append(div)
 }
 
-let boxes = document.querySelectorAll('.box');
-
+let allboxes = document.querySelectorAll(".box")
 function example() {
-    boxes.forEach((ele) => {
-        ele.style.backgroundColor = disco();
-        ele.children[0].innerText = random(500);
-        
+    allboxes.forEach((box) => {
+        box.style.backgroundColor = disco();
+        box.children[0].innerText = random(500);    
     });
 }
 
-container.addEventListener("mousemove", example);
-// let body = document.querySelector('body');
-// let outerBox = document.createElement('div');
-// function disco(){
-//     var x = Math.floor(Math.random() * 256);
-//     var y = Math.floor(Math.random() * 256);
-//     var z = Math.floor(Math.random() * 256);
-//     var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-//     return bgColor;
-// }
-// function random(max){
-//     var num = Math.floor(Math.random() * max);
-//     return num;
-// }
-// outerBox.style.display = "flex";
-// outerBox.style.flexWrap = "wrap"
-// body.append(outerBox);
-// for (let i = 0; i <= 505; i++) {
-//     let box = document.createElement("div");
-//     box.className = "box"
-//     box.style.height = "4rem";
-//     box.style.width = "4rem";
-//     box.style.border = "1px solid black";
-//     let p = document.createElement('p');
-//     p.innerText = random(500);
-//     box.append(p);
-//     outerBox.append(box);
-// }
-// let boxes = document.querySelectorAll(".box");
-// function letExample(){
-//     boxes.forEach((ele) => {
-//         ele.style.backgroundColor = disco();
-//         ele.children[0].innerText = random(500);
-//     })
-// }
-// outerBox.addEventListener("mousemove", letExample);
+boxes.addEventListener("mousemove", example);
